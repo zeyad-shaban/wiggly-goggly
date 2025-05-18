62c4719f442c10100161a36f3dca9b64f27e2320
Zeyad Y. Shaban <64459068+zeyad-shaban@users.noreply.github.com>
2025-05-18 19:35:00 +0300

    defined more pins, added osciliscope

--- code/main.asm
+++ code/main.asm
@@ -2,6 +2,12 @@
 ; Variables / Ports
 ;----------------------------------------
 US_TRIG_R   EQU  P1.1    ; Right Trigger pin
+US_ECHO_R   EQU  P1.0    ; Right Echo pin
+LED_FAR_1   EQU  P2.0    ; First Far LED
+LED_FAR_2   EQU  P2.4    ; Second Far LED
+LED_MID_1   EQU  P2.1    ; First Mid LED
+LED_MID_2   EQU  P2.3    ; Second Mid LED
+LED_CLOSE   EQU  P2.2    ; Close LED
 
 ORG 0000h
 LJMP START 
@@ -35,6 +41,4 @@ MAIN_TOGGLE:
     CLR     US_TRIG_R    ; Drive P1.1 LOW
     ACALL   DELAY_10US   ; Wait ≈10 µs
     SJMP    MAIN_TOGGLE  ; Repeat forever
-
-
 END
--- code/readme.md
+++ code/readme.md
@@ -5,10 +5,10 @@ using https://plit.de/asem-51/ running in DosBox
     mount C E:\workspace\wiggly_goggly\code
     C:
     cd asem5113
-    asem.exe ..\main.asm ..\main.hex
+    asem.exe ..\main.asm ..\main.hex    
 
 
-
+You need to load library for the ultrasonic sensor to work.. uh someone provide link here please ;-;
 Project documentation:
 Distance (cm) = echo (microSec) / 58 
 distance * 58 = echo
